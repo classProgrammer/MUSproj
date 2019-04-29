aba1589f90dbb7ce44e35e32bf5cbe10e20211b1
Gerald Spenlingwimmer <s1810454039@students.fh-hagenberg.at>
2019-04-29 14:46:07 +0200

    added code for person identification based on an image

---
+++ FaceDetection/TestFaceAPI/MyFace.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace TestFaceAPI
+{
+    class MyFace
+    {
+        const string subscriptionKey = "69b5a031d4b24aeda9ff5ace088b784f";
+        const string faceEndpoint = "https://australiaeast.api.cognitive.microsoft.com/";
+
+
+        private readonly IFaceClient faceClient = new FaceClient(
+            new ApiKeyServiceClientCredentials(subscriptionKey),
+            new System.Net.Http.DelegatingHandler[] { });
+
+        private string friendGroup = "myfriends";
+        private string baseImageFolder = "C:/Users/GeraldSpenlingwimmer/Desktop/trainingSet";
+
+        public MyFace()
+        {
+            faceClient.Endpoint = faceEndpoint;
+        }
+
+        public async void Tutorial()
+        {
+            // create a person group
+            await faceClient.PersonGroup.CreateAsync(friendGroup, "My Friends");
+            Console.WriteLine("Group created");
+
+            // create persons
+            var gerald = await faceClient.PersonGroupPerson.CreateAsync(friendGroup, "gerald");
+            var edith = await faceClient.PersonGroupPerson.CreateAsync(friendGroup, "edith");
+            var helmut = await faceClient.PersonGroupPerson.CreateAsync(friendGroup, "helmut");
+
+            Console.WriteLine(gerald.PersonId);
+            Console.WriteLine(edith.PersonId);
+            Console.WriteLine(helmut.PersonId);
+
+            Console.WriteLine("Persons created");
+
+            AddImageToPersonInGroup(friendGroup, gerald.PersonId, baseImageFolder + "/gerald", "Gerald");
+            AddImageToPersonInGroup(friendGroup, edith.PersonId, baseImageFolder + "/edith", "Edith");
+            AddImageToPersonInGroup(friendGroup, helmut.PersonId, baseImageFolder + "/helmut", "Helmut");
+        }
+
+        public async void Identify(string imagePath)
+        {
+            using (Stream s = File.OpenRead(imagePath))
+            {
+                var faces = await faceClient.Face.DetectWithStreamAsync(s);
+                var faceIds = faces.Where(x => x.FaceId.HasValue).Select(face => face.FaceId.Value).ToArray();
+
+                var results = await faceClient.Face.IdentifyAsync(faceIds, friendGroup);
+
+                foreach (var identifyResult in results)
+                {
+                    Console.WriteLine("Result of face: {0}", identifyResult.FaceId);
+                    if (identifyResult.Candidates.ToArray().Length == 0)
+                    {
+                        Console.WriteLine("No one identified");
+                    }
+                    else
+                    {
+                        // Get top 1 among all candidates returned
+                        var candidateId = identifyResult.Candidates[0].PersonId;
+                        var person = await faceClient.PersonGroupPerson.GetAsync(friendGroup, candidateId);
+                        Console.WriteLine("Identified as {0}", person.Name);
+                    }
+                }
+            }
+        }
+
+
+        public async void AddImageToPersonInGroup(string group, Guid personId, string directory, string personName)
+        {
+
+            foreach (string imagePath in Directory.GetFiles(directory, "*.jpg"))
+            {
+                using (Stream s = File.OpenRead(imagePath))
+                {
+                    // Detect faces in the image and add to Anna
+                    await faceClient.PersonGroupPerson.AddFaceFromStreamAsync(friendGroup, personId, s);
+                }
+            }
+            Console.WriteLine($"Images for {personName} added");
+        }
+
+        public async void TrainModel()
+        {
+            // train the groups model
+            await faceClient.PersonGroup.TrainAsync(friendGroup);
+            Console.WriteLine("trained model");
+        }
+
+        public async void TrainingStatus()
+        {
+            TrainingStatus trainingStatus = null;
+            while (true)
+            {
+                trainingStatus = await faceClient.PersonGroup.GetTrainingStatusAsync(friendGroup);
+                if (trainingStatus.Status != TrainingStatusType.Running)
+                {
+                    Console.WriteLine("Train model finished");
+                    break;
+                }
+
+                await Task.Delay(10000);
+            }
+        }
+
+        public async void Reset()
+        {
+            await faceClient.PersonGroup.DeleteAsync(friendGroup);
+            Console.WriteLine($"Deleted Group {friendGroup}");
+        }
+    }
+}
---
+++ FaceDetection/TestFaceAPI/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace TestFaceAPI
+{
+    class Program
+    {
+        const string subscriptionKey = "69b5a031d4b24aeda9ff5ace088b784f";
+        const string detectUri = "https://australiaeast.api.cognitive.microsoft.com/face/v1.0/detect";
+        const string compareTwoFacesUri = "https://australiaeast.api.cognitive.microsoft.com/face/v1.0/verify";
+
+
+
+        static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Assemble the URI for the REST API Call.
+            string uri = detectUri + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(JsonPrettyPrint(contentString));
+                Console.WriteLine("\nPress Enter to exit...");
+            }
+        }
+
+        // Formats the given JSON string by adding line breaks and indents.
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        // Returns the contents of the specified file as a byte array.
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Detect faces:");
+
+            //string imageFilePath = "C:/Users/GeraldSpenlingwimmer/Desktop/1.jpg";
+
+            //if (File.Exists(imageFilePath))
+            //{
+            //    try
+            //    {
+            //        MakeAnalysisRequest(imageFilePath);
+            //        Console.WriteLine("\nWait a moment for the results to appear.\n");
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine("\n" + e.Message + "\nPress Enter to exit...\n");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\nInvalid file path.\nPress Enter to exit...\n");
+            //}
+            //Console.ReadLine();
+
+            var tut = new MyFace();
+            //tut.Reset();
+            
+            //tut.Tutorial();
+            //tut.TrainModel();
+            //tut.TrainingStatus();
+            tut.Identify("C:/Users/GeraldSpenlingwimmer/Desktop/testImages/gerald/2.jpg");
+            while (true) { }
+        }
+    }
+}
